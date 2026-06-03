31de9da7ca63e3c43274cdf5860bde31b9607595
Goreycan <goreyc90@gmail.com>
2026-06-02 20:37:39 -0400

    Base de datos Torneo y model Torneo

---
+++ TorneoTCG/src/main/java/com/example/TorneoTCG/model/Torneo.java
@@ -0,0 +1,47 @@
+package com.example.TorneoTCG.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "torneos")
+public class Torneo {
+
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+
+   @NotBlank(message = "El Torneo debe tener nombre")
+   @Size(min = 3, max = 50)
+   private String nombre;
+
+   @NotNull(message =  "Torneo debe tener fecha de inicio")
+   private LocalDate fechaInicio;
+
+   @NotNull(message =  "Torneo debe tener fecha de fin")
+   private LocalDate fechaFin;
+
+   @NotBlank(message = "El estado de torneo es obligatorio")
+   private String estado;
+
+   @NotNull(message = "El nivel de midiclorianos es obligatorio")
+   @Min(100)
+   private Integer midiclorianos;
+}
---
+++ TorneoTCG/src/main/resources/db/migration/V1_crear_tabla_torneos.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE torneos (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(50) NOT NULL,
+   fechaInicio Date NOT NULL,
+   fechaFin Date NOT NULL,
+   estado VARCHAR NOT NULL,
+   ubicacion VARCHAR(50) NOT NULL,
+   id_recinto INT NOT NUll,  
+)
