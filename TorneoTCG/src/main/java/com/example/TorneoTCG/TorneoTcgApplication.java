8c2bf5772077fbb0c5bf205a795559253cbfef80
pafuentesg <pa.fuentesg@duocuc.cl>
2026-06-23 20:04:09 -0400

    webclient y dependecias?

--- TorneoTCG/src/main/java/com/example/TorneoTCG/TorneoTcgApplication.java
+++ TorneoTCG/src/main/java/com/example/TorneoTCG/TorneoTcgApplication.java
@@ -2,8 +2,11 @@ package com.example.TorneoTCG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class TorneoTcgApplication {
 
 	public static void main(String[] args) {
